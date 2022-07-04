c62b270f26c48c2a97e6112d41e76b584095a9ea
OswaldoCuevas <Oscuevas18@gmail.com>
2022-07-04 12:13:14 -0600

    menu creado (php,css)

--- menu.php
+++ menu.php
@@ -11,6 +11,7 @@
 <script src="https://kit.fontawesome.com/6791667e6d.js" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="css/menu.css">
 <link rel="stylesheet" href="css/padronUsuarios.css">
+<link rel="stylesheet" href="css/CargarArchivo.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <body>
 <div class='dashboard'>
@@ -157,5 +158,9 @@ if(idAux1!=null){
         id=$(this).attr("id");}
         
         );
+</script>
+<script>
+    //cargar archivo
+
 </script>
 </html>
